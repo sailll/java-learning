65b7c23896ea293948d36b0fb339cc9ad503ce2c
sailll choi <sailll@sina.cn>
2016-07-14 12:46:52 +0800

    my array

---
+++ myarray/src/com/geekschool/myarray/Myarray.java
@@ -0,0 +1,18 @@
+/**
+ * Created by caizhongmou on 7/13/16.
+ */
+package com.geekschool.myarray;
+public class Myarray
+{
+    public static void main(String[] argc)
+    {
+        int[] a={1,2,3,4,5};
+        for(int i=0;i<a.length;++i)
+        {
+            System.out.println(a[i]);
+        }
+
+    }
+
+
+}
