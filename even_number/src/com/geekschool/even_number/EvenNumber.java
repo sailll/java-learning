362df81a4303d5352504480ffde43581af91be46
sailll choi <sailll@sina.cn>
2016-07-14 12:47:18 +0800

    print the even number

---
+++ even_number/src/com/geekschool/even_number/EvenNumber.java
@@ -0,0 +1,15 @@
+/**
+ * Created by caizhongmou on 7/13/16.
+ */
+package com.geekschool.even_number;
+public class EvenNumber
+{
+    public static void main(String[] argc)
+    {
+        for(int i=0;i<=100;i+=2)
+        {
+            System.out.print(i+" ");
+        }
+    }
+
+}
